4c11ef915138a37d4a0d5d62b73e6fd8e232f15b
Ayushparui <paruiayush900@gmail.com>
2022-07-01 11:04:09 +0530

    Added procfile

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT;
+if(PORT == null || PORT == ""){
+    PORT = 3000;
+}
 const _ = require("lodash")
 // console.log(date())
 
